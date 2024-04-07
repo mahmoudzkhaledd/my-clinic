cb5d9a4e737261d58a83650351b72a473e02687e
Mahmoud Khaled <78917638+mahmoudzkhaledd@users.noreply.github.com>
2024-04-07 14:34:19 +0200

    clinic card hr

--- app/dashboard/clinics/_components/ClinicCard.tsx
+++ app/dashboard/clinics/_components/ClinicCard.tsx
@@ -6,17 +6,18 @@ import Link from 'next/link'
 import React from 'react'
 
 export default function ClinicCard({ clinic }: { clinic: Clinic }) {
+    // hover:bg-gray-50 dark:hover:bg-muted
     return (
         <Link target='__blank' href={`/clinic-dashboard/${clinic.id}`}>
-            <Card className='w-full h-full cursor-pointer hover:bg-gray-50 dark:hover:bg-muted transition-colors'>
+            <Card className='w-full h-full cursor-pointer   hover:scale-[1.02] group transition-all'>
                 <CardHeader className='pb-4'>
                     <div className='flex items-center justify-between '>
                         <h2 className=' text-lg font-semibold capitalize'>{clinic.name}</h2>
-                        <ArrowRightCircle className=' w-5 h-5 text-gray-500' />
+                        <ArrowRightCircle className='group-hover:translate-x-1 transition-all w-5 h-5 text-gray-500' />
                     </div>
                 </CardHeader>
               
-                <hr className='mb-5 border-gray-700'/>
+                <hr className='mb-5 border-muted'/>
                 <CardContent className=' space-y-2 break-words'>
                     <p className=' text-gray-400 text-sm'>
                         Email: {clinic.contactEmail}
